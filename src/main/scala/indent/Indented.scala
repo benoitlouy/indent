55f97934cffc52fc441e6f214551773843ff135a
Benoit Louy <benoit.louy@fastmail.com>
2020-08-23 17:17:04 -0400

    handle empty sequence of Indented

--- src/main/scala/indent/Indented.scala
+++ src/main/scala/indent/Indented.scala
@@ -8,6 +8,7 @@ object Element {
   case object NewLine extends Element
   case object AddIndent extends Element
   case object RemoveIndent extends Element
+  case object RemoveLineIfEmpty extends Element
 }
 
 final class Indented(private[indent] val content: Vector[Element]) extends AnyVal {
@@ -15,19 +16,37 @@ final class Indented(private[indent] val content: Vector[Element]) extends AnyVa
   def indent(implicit indent: Indentation): String = indentWith(indent.value)
 
   def indentWith(indentation: String): String = {
-    val res = content
-      .foldLeft((0, true, "", "")) {
-        case ((count, false, line, acc), Element.String(str)) =>
-          (count, false, s"$line$str", acc)
-        case ((count, true, line, acc), Element.String(str)) =>
-          (count, false, s"$line${indentation * count}$str", acc)
-        case ((count, _, line, acc), Element.NewLine) =>
-          (count, true, "", s"$acc$line\n")
-        case ((count, nl, line, acc), Element.AddIndent) => (count + 1, nl, line, acc)
-        case ((count, nl, line, acc), Element.RemoveIndent) => (if (count == 0) 0 else count - 1, nl, line, acc)
-      }
+    val (curCount, _, _, line, acc, empty) = content
+    // current line indent level
+    // indent level count
+    // follow insertion of a new line
+    // line being built
+    // previous lines
+    // line should not be inserted if true
+      .foldLeft((0, 0, true, "", "", false)) {
+        case ((curCount, count, _, line, acc, _), Element.RemoveLineIfEmpty) => (curCount, count, false, line, acc, true)
 
-    s"${res._4}${res._3}"
+        case ((curCount, count, _, line, acc, _), Element.String(str)) =>
+          (curCount, count, false, s"$line$str", acc, false)
+
+        case ((_, count, _, "", acc, empty), Element.NewLine) =>
+          (count, count, true, "", if (empty) acc else s"$acc\n", false)
+
+        case ((curCount, count, _, line, acc, _), Element.NewLine) =>
+          (count, count, true, "", s"$acc${indentation * curCount}$line\n", false)
+
+        case ((curCount, count, true, line, acc, empty), Element.AddIndent) =>
+          (curCount + 1, count + 1, true, line, acc, empty)
+
+        case ((curCount, count, false, line, acc, empty), Element.AddIndent) => (curCount, count + 1, false, line, acc, empty)
+
+        case ((curCount, count, true, line, acc, empty), Element.RemoveIndent) => (curCount - 1, if (count == 0) 0 else count - 1, true, line, acc, empty)
+
+        case ((curCount, count, false, line, acc, empty), Element.RemoveIndent) => (curCount, if (count == 0) 0 else count - 1, false, line, acc, empty)
+      }
+    val lastLine = if (empty && line.isEmpty) "" else s"${indentation * curCount}$line"
+    val previousLines = if (empty && line.isEmpty && acc.lastOption.exists(_ == '\n')) acc.dropRight(1) else acc
+    s"$previousLines$lastLine"
   }
 
   override def toString: String = indentWith("•")
