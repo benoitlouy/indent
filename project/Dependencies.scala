340c16f1a2c3bd7bce493c53daaf2ede3c520488
Benoit Louy <benoit.louy@fastmail.com>
2020-10-22 01:26:21 +0000

    update sbt and dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,5 @@ import sbt._
 object Dependencies {
   lazy val munit = "org.scalameta" %% "munit" % "0.7.14"
 
-  lazy val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.3.1-RC3"
-
-  lazy val semanticdbScalac = "org.scalameta" % "semanticdb-scalac" % "4.3.18"
+  lazy val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.4.3"
 }
