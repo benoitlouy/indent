cd2d63389796cf4d594ac337e9328281752161f0
Benoit Louy <benoit.louy@fastmail.com>
2024-04-27 13:04:36 -0400

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    typelevel-nix = {
+      url = "github:typelevel/typelevel-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.follows = "typelevel-nix/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlay ];
+        };
+        jdk = pkgs.jdk8;
+      in
+      {
+        devShell = pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          name = "scala shell for indent project";
+          typelevelShell = {
+            jdk.package = jdk;
+            sbtMicrosites = {
+              enable = false;
+            };
+          };
+        };
+      }
+    );
+}
